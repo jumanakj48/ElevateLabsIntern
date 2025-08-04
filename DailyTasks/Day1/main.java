e334d846bf391c6a86641843c6f2586a5cfbd963
jumanakj48 <kalimathjumana.cs21@bitsathy.ac.in>
2025-08-04 16:19:38 +0530

    first commit

---
+++ DailyTasks/Day1/main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+
+        System.out.println("------Calculator-------");
+        System.out.println("Enter your choice");
+        System.out.println("1. Addition");
+        System.out.println("2. Substraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        
+    }
+}
